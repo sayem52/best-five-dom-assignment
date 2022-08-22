8ff081524f993020ff656b2656e5825feb718dff
sayem52 <sayem15-12949@diu.edu.bd>
2022-08-23 04:20:00 +0600

    third

---
+++ index.js
@@ -0,0 +1,184 @@
+  
+  const btn1=document.getElementById("hak-sel");
+  btn1.addEventListener("click", function(){
+            
+        let addName=getName("hakimi");
+        let fiveList=document.getElementById("five-list");
+        
+        const li=document.createElement('li');
+        li.innerText=addName;    
+        fiveList.appendChild(li);
+        disableButton("hak-sel");
+    
+        });
+
+const btn2=document.getElementById("ney-sel");
+btn2.addEventListener("click", function(){
+                 
+    let addName=getName("neymar");
+    let fiveList=document.getElementById("five-list");
+                
+    const li=document.createElement('li');
+    li.innerText=addName;    
+    fiveList.appendChild(li);
+    disableButton("ney-sel");
+         
+     });
+
+const btn3=document.getElementById("mes-sel");
+btn3.addEventListener("click", function(){
+                
+    let addName3=getName("messi");
+    let fiveList=document.getElementById("five-list");
+                
+    const li=document.createElement('li');
+    li.innerText=addName3;    
+    fiveList.appendChild(li);
+    disableButton("mes-sel");
+        
+    });
+
+const btn4=document.getElementById("ica-sel");
+btn4.addEventListener("click", function(){
+                 
+    let addName=getName("icardi");
+    let fiveList=document.getElementById("five-list");
+                
+    const li=document.createElement('li');
+    li.innerText=addName;    
+    fiveList.appendChild(li);
+    disableButton("ica-sel");
+         
+     });
+
+const btn5=document.getElementById("kim-sel");
+btn5.addEventListener("click", function(){
+                 
+    let addName=getName("kim");
+    let fiveList=document.getElementById("five-list");
+                
+    const li=document.createElement('li');
+    li.innerText=addName;    
+    fiveList.appendChild(li);
+    disableButton("kim-sel");
+         
+     });
+
+
+const btn6=document.getElementById("mar-sel");
+btn6.addEventListener("click", function(){
+                 
+    let addName=getName("mar");
+    let fiveList=document.getElementById("five-list");
+                
+    const li=document.createElement('li');
+    li.innerText=addName;    
+    fiveList.appendChild(li);
+    disableButton("mar-sel");
+         
+     });
+
+
+const btn7=document.getElementById("nav-sel");
+btn7.addEventListener("click", function(){
+                 
+    let addName=getName("navas");
+    let fiveList=document.getElementById("five-list");
+                
+    const li=document.createElement('li');
+    li.innerText=addName;    
+    fiveList.appendChild(li);
+    disableButton("nav-sel");
+         
+     });
+
+const btn8=document.getElementById("ser-sel");
+btn8.addEventListener("click", function(){
+                 
+    let addName=getName("ser");
+    let fiveList=document.getElementById("five-list");
+                
+    const li=document.createElement('li');
+    li.innerText=addName;    
+    fiveList.appendChild(li);
+    disableButton("ser-sel");
+         
+     });
+
+
+const btn9=document.getElementById("ver-sel");
+btn9.addEventListener("click", function(){
+                 
+    let addName=getName("ver");
+    let fiveList=document.getElementById("five-list");
+                
+    const li=document.createElement('li');
+    li.innerText=addName;    
+    fiveList.appendChild(li);
+    disableButton("ver-sel");
+         
+     });
+
+
+//      const btn2=document.getElementById("hak-sel");
+// hakBtn.addEventListener("click", function(){
+                 
+//     let addName=getName("hakimi");
+//     let fiveList=document.getElementById("five-list");
+                
+//     const li=document.createElement('li');
+//     li.innerText=addName;    
+//     fiveList.appendChild(li);
+//     disableButton("hak-sel");
+         
+//      });
+
+
+function getName(id){
+    const getPlayerName=document.getElementById(id);
+    let playerName=getPlayerName.innerText;
+
+    return playerName;
+}
+
+
+
+function disableButton(id){
+    const disableB=document.getElementById(id);
+
+    return disableB.disabled = true;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Function for input convert to number
+function inputTOInt(id){
+    const wantAmount=document.getElementById(id).value;
+    const wantNumber=parseInt(wantAmount);
+
+    return wantNumber;
+
+}
+
+
+//  // Function total
+function getTotal(id,cNumber){
+    const Amount=document.getElementById(id).innerText;
+    const balanceNumber=parseFloat(Amount);
+
+    const totalBalance=cNumber + balanceNumber ;
+
+    document.getElementById(id).innerText=totalBalance;
+
+}
